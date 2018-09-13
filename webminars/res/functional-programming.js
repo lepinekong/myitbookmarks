1438ca7b261f437c423049426aa2cf30bc6414a5
lepkon <lepkon@free.fr>
2018-09-13 16:04:32 +0200

    webminars functional programming

---
+++ webminars/res/functional-programming.js
@@ -0,0 +1,17 @@
+        videos = [
+            {
+                "id": "m40YOZr1nxQ",
+                "title": "Let’s Code Real World App Using Purely Functional Techniques",
+                "description": "We will create a small, real-world data transformation program that demonstrates how functional techniques address issues such as control flow, type construction, data validation, etc. Through demonstrate-by-example, we'll explore the railway pattern, monadic chaining, disjunctions, applicatives, and currying."
+            },            
+            {
+                "id": "US8QG9I1XW0",
+                "title": "Functional architecture - The pits of success - Mark Seemann",
+                "description": " Instead of deliberate design, functional programming forms pits of success where you naturally fall into the same ‘best practices’ that you have to deliberately work for in object-oriented programming. In this session, you’ll learn about a handful of such ‘best practices’, and how functional programming automatically lead you there, without your explicit effort."
+            },
+            {
+                "id": "E8I19uA-wGY",
+                "title": "Functional programming design patterns by Scott Wlaschin",
+                "description": "In object-oriented development, we are all familiar with design patterns such as the Strategy pattern and Decorator pattern, and design principles such as SOLID. The functional programming community has design patterns and principles as well."
+            }            
+        ]
